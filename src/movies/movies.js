49274806a8f4234e8f531191f3c9ba58430c4b2b
MiguelRodriguezSerrano <50631393+MiguelRodriguezSerrano@users.noreply.github.com>
2019-07-04 13:53:14 +0200

    terminado el CRUD

---
+++ src/movies/movies.js
@@ -0,0 +1,65 @@
+const { Router } = require('express');
+const router = Router();
+//Install underscore library
+//const _ = require('underscore')
+
+const movies = require('./movies.json')
+
+//Show all the movies
+router.get('/', (req, res) => {
+    res.json(movies)
+});
+
+//Add a new movie
+router.post('/', (req, res) => {
+    const {title, director, year, rate} = req.body;
+    if (title && director && year && rate) {
+        const id = movies.length +1
+        const newMovie = {...req.body, id};
+        //console.log(newMovie)
+        movies.push(newMovie)
+        res.json(movies)
+    } else {
+        res.status(500).json({error: 'There was an error.'})
+    }
+});
+
+
+//Removing a movie
+ router.delete('/:id', (req, res) => {
+    const idToRemove  = req.params.id;
+
+  
+    for (let i = 0; i < movies.length; i++) {
+        if(movies[i].id == idToRemove){
+            movies.splice(i, 1);
+       
+              } else {
+                res.status(404).send("No se ha encontrado ninguna película con ese ID.")
+            }
+
+    }
+    res.json(movies);
+}); 
+
+
+//Update a movie
+router.put('/:id', (req, res) => {
+    const movieToUpdateId = req.params.id;
+    const updatedMovie = req.body;
+    for (let i = 0; i < movies.length; i++) {
+     {
+         if(movies[i].id == movieToUpdateId) {
+            movies[i].title = updatedMovie.title ||  movies[i].title;
+            movies[i].director = updatedMovie.director ||  movies[i].director;
+            movies[i].year = updatedMovie.year ||  movies[i].year ;
+            movies[i].rate = updatedMovie.rate ||  movies[i].rate ; 
+         }
+        }
+        
+    }
+    res.json(movies);
+});
+
+
+module.exports = router;
